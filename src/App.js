47e9b2ed2be0529c30eaeddddf5c13d6f0e19201
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-08-28 23:04:26 -0400

    Created dictionary component pages and added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./aubrey-justine.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          This site was coded with 🖤 by Aubrey Jamison.
+          {/*  Add Github link and linkedin profile */}
+        </footer>
+      </div>
     </div>
   );
 }
