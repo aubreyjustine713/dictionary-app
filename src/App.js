4d795e4270f324f4e0f4e03ea872087f274b5855
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-09-18 23:08:14 -0400

    Small changes to styling, default keyword added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           <h1>Dictionary App</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="woodland" />
         </main>
         <footer className="app-footer">
           This site was coded with 🤍 by{" "}
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,32 +3,43 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
           <input type="search" onChange={handleKeywordChange} />
         </form>
+        <div className="hint">
+          suggested words: sunset, wine, yoga, forest...
+        </div>
         <Results results={results} />
-      </section>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
 }
