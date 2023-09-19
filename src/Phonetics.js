7e7590a12628f9eec07fe634edac2ac17586a63d
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-09-18 21:16:03 -0400

    Changed the way Phonetics functions

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  let word = props.props.word;
-  let audioUrl = `https://api.dictionaryapi.dev/media/pronunciations/en/${word}-us.mp3`;
-  return (
-    <div className="Phonetics">
-      <a href={audioUrl} target="_blank" rel="noreferrer">
-        Listen 🔊
-      </a>
-      <br />
-      {props.props.phonetics[0].text}
-    </div>
-  );
+  let audio = props.props.phonetics[0].audio;
+  if (audio === "") {
+    return `${props.props.phonetics[0].text}`;
+  } else {
+    return (
+      <div className="Phonetics">
+        <a
+          href={props.props.phonetics[0].audio}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Listen 🔊
+        </a>
+        <br />
+        {props.props.phonetics[0].text}
+      </div>
+    );
+  }
 }
